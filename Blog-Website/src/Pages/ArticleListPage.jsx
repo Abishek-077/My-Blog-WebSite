b440f5fb71505dae3e61c9c8821ff445dba851f9
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-19 12:17:00 +0545

    creating a articleListPage

--- Blog-Website/src/Pages/ArticleListPage.jsx
+++ Blog-Website/src/Pages/ArticleListPage.jsx
@@ -1,6 +1,12 @@
+import articles from "../../Article-Content";
+import ArticlesList from "../ArticlesList";
+
 export default function ArticleListPage() {
     return (
-        <h1>This is ArtcilesListPage.....</h1>
-    );
+        <>
+            <h1>Articles</h1>
+            <ArticlesList articles={articles} />
 
+        </>
+    );
 }
