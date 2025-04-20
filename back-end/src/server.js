76c99ad8929b827af1d80c86cfc97a6fcd145cac
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-20 12:31:42 +0545

    working on my blogs upvote and comment for backend

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 
+const articlesInfo = [
+    { name: 'learn-node', upvotes: 0 },
+    { name: 'learn-react', upvotes: 0 },
+    { name: 'mongodb', upvotes: 0 },
+]
 const app = express();
 
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
-app.get('/hello', function (req, res) {
-    res.send('hello send the res and this the response that you get ');
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const article = articlesInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1;
+
+    res.send('Success!' + req.params.name + 'now has' + article.upvotes + 'upvotes');
 });
 
-app.post('/hello', function (req, res) {
-    res.send('hello,' + req.body.name + ' from the post endpoint');
-});
+// Start the server
 app.listen(8000, function () {
     console.log('Server is Listening on port 8000');
-
 });
