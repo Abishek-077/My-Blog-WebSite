7dd7d86237cc9529de8970bd70c1dd82ce39bf5a
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-20 14:01:34 +0545

    adding mangodb as database to express server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,20 +1,55 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const articlesInfo = [
-    { name: 'learn-node', upvotes: 0 },
-    { name: 'learn-react', upvotes: 0 },
-    { name: 'mongodb', upvotes: 0 },
-]
+    { name: 'learn-node', upvotes: 0, comments: [] },
+    { name: 'learn-react', upvotes: 0, comments: [] },
+    { name: 'mongodb', upvotes: 0, comments: [] },
+];
+
 const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const uri = 'mongodb://127.0.0.1:27017'; // ✅ colon (:) instead of dot (.)
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+
+    const db = client.db('my-blog-website-db');
+
+    const article = await db.collection('articles').findOne({ name });
+
+    res.json(article);
+});
+
+// Upvote route
 app.post('/api/articles/:name/upvote', (req, res) => {
     const article = articlesInfo.find(a => a.name === req.params.name);
     article.upvotes += 1;
+    res.json(article);
+});
 
-    res.send('Success!' + req.params.name + 'now has' + article.upvotes + 'upvotes');
+// Comment route
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+    article.comments.push({ postedBy, text });
+
+    res.json(article);
 });
 
 // Start the server
