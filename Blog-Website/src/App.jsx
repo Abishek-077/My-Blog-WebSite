dd1a9326eb127ae0ca0580ccac8e374ed4c007fa
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-21 08:52:12 +0545

    displaying the comments from the server

--- Blog-Website/src/App.jsx
+++ Blog-Website/src/App.jsx
@@ -1,12 +1,11 @@
 import AboutPage from './Pages/AboutPage'
 import ArticlesListPage from './Pages/ArticlesListPage'
 import HomePage from './Pages/HomePage'
-import ArticlePage from './Pages/ArticlePage'
+import ArticlePage, { loader as articleLoader } from './Pages/ArticlePage'
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
 import './App.css'
 import Layout from './Layout'
 import PageNotFound from './PageNotFound'
-import axios from 'axios'
 
 const routes = [{
   path: '/',
@@ -23,17 +22,12 @@ const routes = [{
   },
   {
     path: '/articles',
-    element: <ArticlesListPage />
+    element: <ArticlesListPage />,
   },
   {
     path: '/articles/:name',
     element: <ArticlePage />,
-    loader: async function () {
-      const response = await axios.get('/api/articles/learn-node');// make it think they are running on same port 
-      const { upvotes, comments } = response.data;
-      return { upvotes, comments };
-
-    }
+    loader: articleLoader,
   }
   ]
 
--- Blog-Website/vite.config.js
+++ Blog-Website/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://z0p40nv6-8000.inc1.devtunnels.ms',
+        changeOrigin: true,
+      }
+    }
+  }
 })
