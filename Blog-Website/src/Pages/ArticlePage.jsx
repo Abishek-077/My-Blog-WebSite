54881cb8874090aa27026380e67c9aa0547262c1
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-21 09:19:23 +0545

    conneting upvote feature to my blog which is fetched from server call

--- Blog-Website/src/Pages/ArticlePage.jsx
+++ Blog-Website/src/Pages/ArticlePage.jsx
@@ -1,19 +1,30 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import articles from "../article-content";
 import CommentsList from "../commentsList";
+import { useState } from "react";
 import axios from 'axios';
 
 export default function ArticlePage() {
     const { name } = useParams();
-    const { upvotes, comments } = useLoaderData();
+    const { upvotes: initialUpvotes, comments } = useLoaderData();
+    const { upvotes, setUpvotes } = useState(initialUpvotes);
+
 
 
 
     const article = articles.find((a) => a.name === name);
 
+    async function onUpvoteClicked() {
+        const response = await axios.post('/api/articles' + name + '/upvote');
+        const updatedArticleData = response.data;
+        setUpvotes(updatedArticleData.upvotes);
+
+    }
+
     return (
         <>
             <h1>{article.title}</h1>
+            <button onClick={onUpvoteClicked}>Upvote</button>
             <p>This article has{upvotes} upvotes!</p>
             {article.content.map(p => <p key={p}>{p}</p>)}
             <CommentsList comments={comments} />
