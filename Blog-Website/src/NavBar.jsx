6a055b09a80184ddc7a198c7755a8159c46b572e
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-23 13:33:37 +0545

    Authentaciting both login and signup page with firbase with some features

--- Blog-Website/src/NavBar.jsx
+++ Blog-Website/src/NavBar.jsx
@@ -1,7 +1,12 @@
+import { getAuth, signOut } from 'firebase/auth';
 import './NavBar.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useUser from './useUser';
 
 export default function NavBar() {
+    const { isLoading, user } = useUser();
+    const navigate = useNavigate();
+
     return (
         <>
             <ul>
@@ -16,7 +21,26 @@ export default function NavBar() {
                 <li>
                     <Link to={'/articles'}>ArticlesPage</Link>
                 </li>
+
+                {isLoading ? (
+                    <li>Loading......</li>
+                ) : (
+                    <>
+                        {user && (
+                            <li>
+                                <p>Logged in as {user.email}</p>
+                            </li>
+                        )}
+
+                        <li>
+                            {user
+                                ? <button onClick={() => signOut(getAuth())}>Log Out</button>
+                                : <button onClick={() => navigate('/login')}>Log In</button>
+                            }
+                        </li>
+                    </>
+                )}
             </ul>
         </>
-    )
+    );
 }
