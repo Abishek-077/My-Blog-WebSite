18b08da3422b40543f5032085035a9eca79a7f54
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-23 11:46:01 +0545

    creating a login page

--- Blog-Website/src/Pages/LoginPage.jsx
+++ Blog-Website/src/Pages/LoginPage.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
 export default function LoginPage() {
+    const { email, setEmail } = useState('');
+    const { password, setPassword } = useState('');
+    const { error, setError } = useState('');
+
+    const navigate = useNavigate();
+    async function logIn() {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+
+        } catch (e) {
+            setError(e.message);
+
+        }
+
+    }
+
     return (
-        <h1>This is a Login Page...../</h1>
+        <>
+            <h1>Log In</h1>
+            {error && <p>{error}</p>}
+            <input
+                placeholder='Enter email address'
+                value={email}
+                onChange={e => setEmail(e.target.value)} />
+
+            <input
+                placeholder='Enter your password'
+                value={password}
+                onChange={e => setPassword(e.target.value)} />
+            <button onClick={logIn}>Log In</button>
+            <Link to={'/createAccount'}>Don't have an account? Create one here</Link>
+        </>
+
+
     );
+
 
 
 }
